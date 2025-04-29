1ff68266ba4992a784d0ac6a8ff495c0c546795a
yskim9809 <dudtj9809@gmail.com>
2025-04-30 02:02:00 +0900

    20250430

---
+++ programmers/src/programmers/MakingHamburgers.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Stack;
+
+public class MakingHamburgers {
+	class Solution {
+		public int solution(int[] ingredient) {
+			int answer = 0;
+			Stack<Integer> burger = new Stack<>();
+			for (int i = 0; i < ingredient.length; i++) {
+				burger.push(ingredient[i]);
+				if (burger.size() >= 4) {
+					if (burger.get(burger.size() - 4) == 1 && burger.get(burger.size() - 3) == 2
+							&& burger.get(burger.size() - 2) == 3 && burger.get(burger.size() - 1) == 1) {
+						answer++;
+						burger.pop();
+						burger.pop();
+						burger.pop();
+						burger.pop();
+					}
+				}
+			}
+			return answer;
+		}
+	}
+}
